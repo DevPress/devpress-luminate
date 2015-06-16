cccaca25d25caffe5a487403d473c0b7cad697d2
Devin Price <devin@wptheming.com>
2015-06-16 12:39:28 -0500

    Display comment labels since placeholders are not used.

--- comments.php
+++ comments.php
@@ -50,7 +50,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
-					'avatarluminateize' => 50
+					'avatar_size' => 50
 				) );
 			?>
 		</ol><!-- .comment-list -->
