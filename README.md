1bcb46d042322bc3dec662f40dd4bc88a1145e31
Devin Price <devin@wptheming.com>
2015-07-12 12:49:40 -0500

    Fix search archives.

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ Development
 * Enhancement: Support for JetPack Infinite Scroll
 * Enhancement: Support for JetPack Testimonials
 * Enhancement: Better menu support when admin menu is active
+* Fix: Search archive now displays correctly
 
 1.0.2
 ---
--- search.php
+++ search.php
@@ -7,26 +7,19 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<header class="page-header">
+		<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-				?>
+				<?php get_template_part( 'partials/content', 'search' ); ?>
 
 			<?php endwhile; ?>
 
@@ -34,12 +27,13 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part( 'partials/content', 'none' ); ?>
 
 		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
